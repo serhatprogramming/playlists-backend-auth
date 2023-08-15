7e3d86e0f7d53af87c410cba8803b6887ab50fd0
serhatprogramming <serhatprogramming@gmail.com>
2023-08-15 18:39:28 -0400

    jwt authentication

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const config = require("./utils/config");
 
 const playlistsRouter = require("./controllers/playlists");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/playlists", playlistsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
